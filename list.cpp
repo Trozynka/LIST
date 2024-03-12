2172c8563d70d2dedf5735c85431c65c9bbd332a
Trozynka <160461087+Trozynka@users.noreply.github.com>
2024-03-12 10:56:43 +0300

    Add files via upload

---
+++ list.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <list>
+using namespace std;
+
+void show(list<int>& _lst)
+{
+    for (auto i : _lst) {
+        cout << i << ' ';
+    }
+}
+
+int main()
+{
+    list<int> lst{};
+
+    int i = 0;
+
+    for (i; i < 100; i++)
+    {
+        int tmp = rand() % 200;
+        lst.push_back(tmp);
+    }
+
+    lst.push_front(202);
+    lst.push_front(201);
+    lst.push_back(203);
+    show(lst);
+
+
+    cout << endl;
+
+
+}
+
